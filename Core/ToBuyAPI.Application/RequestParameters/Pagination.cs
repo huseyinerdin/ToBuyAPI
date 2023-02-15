ae94aa557a52b18a231a5454d9e7a9dfb6b44cdb
huseyinerdin <erdinh61@gmail.com>
2023-02-16 01:07:01 +0300

    pagination-class-added

---
+++ Core/ToBuyAPI.Application/RequestParameters/Pagination.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToBuyAPI.Application.RequestParameters
+{
+    public record Pagination
+    {
+        public int PageIndex { get; set; } = 0;
+        public int PageSize { get; set; } = 5;
+    }
+}
