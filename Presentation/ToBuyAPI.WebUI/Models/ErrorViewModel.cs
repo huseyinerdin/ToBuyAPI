b8e367ce67b4025d3f9d38f219ae61be8df376db
Hüseyin Erdin <erdinh61@gmail.com>
2023-02-28 14:54:49 +0300

    Delete Presentation/ToBuyAPI.WebUI directory

--- Presentation/ToBuyAPI.WebUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ToBuyAPI.WebUI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
